a222582f9e05a258a11ef155ec4b589ac3f7136b
Emmanuel <emmanuel.nocum@cvsu.edu.ph>
2023-01-05 21:53:08 +0800

    too do

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
-
+//todo
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
     <h1>Im, Emmanuel Nocum</h1>
